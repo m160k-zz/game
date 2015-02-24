5d62c724083ce5849ff3a48bf28992426dd5c367
Iliqna <iliyana.dermendzhieva@gmail.com>
2015-02-24 19:19:45 +0200

    init

---
+++ src/main/java/org/game/util/GameUtil.java
@@ -0,0 +1,11 @@
+package org.game.util;
+
+import java.util.List;
+import java.util.Random;
+
+//public static Question randomQuest(List<Question> list) {
+//	  int random = new Random().nextInt(list.size());
+//	  Question q = list.get(random);
+//	  return q;
+//	 }
+//}
